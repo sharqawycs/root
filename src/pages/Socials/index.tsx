9f46dc436a76a02c0fa8b18fd954226897048f02
sharqawycs <sharqawycs@gmail.com>
2025-08-06 02:00:39 +0300

    add prettier for code formatting

--- src/pages/Socials/index.tsx
+++ src/pages/Socials/index.tsx
@@ -5,26 +5,17 @@ export default function Socials() {
             <p>All the places you can find me online.</p>
 
             <div class="social-links">
-                <a
-                    href="https://github.com/yourusername"
-                    target="_blank"
-                    rel="noopener noreferrer">
+                <a href="https://github.com/yourusername" target="_blank" rel="noopener noreferrer">
                     <h3>GitHub</h3>
                     <p>Code and projects</p>
                 </a>
 
-                <a
-                    href="https://twitter.com/yourusername"
-                    target="_blank"
-                    rel="noopener noreferrer">
+                <a href="https://twitter.com/yourusername" target="_blank" rel="noopener noreferrer">
                     <h3>Twitter</h3>
                     <p>Random thoughts</p>
                 </a>
 
-                <a
-                    href="https://linkedin.com/in/yourusername"
-                    target="_blank"
-                    rel="noopener noreferrer">
+                <a href="https://linkedin.com/in/yourusername" target="_blank" rel="noopener noreferrer">
                     <h3>LinkedIn</h3>
                     <p>Professional stuff</p>
                 </a>
