46ecc5fd4e2e530665684f0a1808e6152da42616
sharqawycs <sharqawycs@gmail.com>
2025-08-12 12:28:57 +0300

    remove unused useMarkdownToHTML utility function

--- personal-site/src/utils/useMarkdownToHTML.ts
+++
@@ -1,8 +0,0 @@
-import fs from 'fs';
-import markdownToHTML from '@/utils/markdownToHTML';
-
-export default function useMarkdownToHTML(filePath: string) {
-    const markdown = fs.readFileSync(filePath, 'utf-8');
-    const html = markdownToHTML(markdown);
-    return html;
-}
