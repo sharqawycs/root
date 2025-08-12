6d7dcb645dfe9d6a43dd4ea8bc4aaed81767173f
sharqawycs <sharqawycs@gmail.com>
2025-08-12 15:39:45 +0300

    Enable adding meta data to blog notes #5

--- personal-site/src/utils/services/JournalServices.ts
+++ personal-site/src/utils/services/JournalServices.ts
@@ -1,27 +1,61 @@
-import { slugify, titleize } from '@/utils/slugify';
+import { slugify, titleize, dateify } from '@/utils/slugify';
 
 const files = import.meta.glob<{ default: string }>('@/data/journals/*.md', { query: '?raw', eager: true });
 
-interface Journal {
+/* TYPES */
+interface JournalInfo {
     title: string;
     slug: string;
+    date: string;
 }
 
-interface JournalWithContent extends Journal {
+interface Journal extends JournalInfo {
     content: string;
 }
 
-const journals: JournalWithContent[] = Object.entries(files).map(([path, mod]) => {
+/* HELPERS */
+// Sorts from new to old and expect date like ddmmyy
+const sortJournalsByDate = (arr: Journal[]): Journal[] => {
+    // New to Old
+    return arr.sort((b, a) => {
+        const ay = parseInt(a.date.slice(4, 6), 10) + 2000;
+        const by = parseInt(b.date.slice(4, 6), 10) + 2000;
+        if (ay !== by) return ay - by;
+
+        const am = parseInt(a.date.slice(2, 4), 10);
+        const bm = parseInt(b.date.slice(2, 4), 10);
+        if (am !== bm) return am - bm;
+
+        const ad = parseInt(a.date.slice(0, 2), 10);
+        const bd = parseInt(b.date.slice(0, 2), 10);
+
+        return ad - bd;
+    });
+};
+
+// The journals full array from data files
+const journals: Journal[] = Object.entries(files).map(([path, mod]) => {
     const content = mod.default;
-    const fileName = path.split('/').pop()!;
-    const slug = slugify(fileName.replace(/\.md$/, ''));
+    const fileName = path.split('/').pop()!; // "File name110925.md"
+    const withoutExt = fileName.replace(/\.md$/, '');
+
+    const [slug, date] = [slugify(withoutExt.slice(0, -6)), withoutExt.slice(-6)];
     const title = titleize(slug);
 
-    return { title, slug, content };
+    return { title, slug, date, content };
 });
 
-export const getAllJournals = (): JournalWithContent[] => journals;
+const sortedJournals: Journal[] = sortJournalsByDate(journals);
+const datefiedJournals: Journal[] = sortedJournals.map(journal => ({
+    ...journal,
+    date: dateify(journal.date),
+}));
 
-export const getJournalBySlug = (slug: string): JournalWithContent | undefined => getAllJournals().find(journal => journal.slug === slug);
+const getAllJournals = (): Journal[] => datefiedJournals;
 
-export const getAllTitles = (): Journal[] => getAllJournals().map(journal => ({ title: journal.title, slug: journal.slug }));
+const getJournalBySlug = (slug: string): Journal | undefined => getAllJournals().find(journal => journal.slug === slug);
+
+const getAllJournalsInfo = (): JournalInfo[] =>
+    getAllJournals().map(journal => ({ title: journal.title, slug: journal.slug, date: journal.date }));
+
+export { getJournalBySlug, getAllJournalsInfo };
--- personal-site/src/utils/slugify.ts
+++ personal-site/src/utils/slugify.ts
@@ -17,3 +17,14 @@ export const titleize = (text: string): string => {
         .replace(/-/g, ' ')
         .replace(/(?:^|\s)\w/g, match => match.toUpperCase());
 };
+
+export const dateify = (date: string): string => {
+    if (!/^\d{6}$/.test(date)) throw new Error('Invalid date format');
+
+    const day = date.slice(0, 2);
+    const month = parseInt(date.slice(2, 4), 10) - 1; // months are 0-based
+    const year = parseInt(date.slice(4, 6), 10) + 2000; // 20xx
+
+    const datefied = new Date(year, month, parseInt(day, 10));
+    return datefied.toLocaleDateString('en-GB', { day: '2-digit', month: 'short', year: 'numeric' });
+};
