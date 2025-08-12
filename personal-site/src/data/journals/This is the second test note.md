876921a99594e01310b58dcd3263061fee280ffc
sharqawycs <sharqawycs@gmail.com>
2025-08-12 14:14:29 +0300

    Blog/Journal System Done #3

---
+++ personal-site/src/data/journals/This is the second test note.md
@@ -0,0 +1,3 @@
+# Also I am a test note
+
+Sorry man but I am not sure about this
--- personal-site/src/pages/Journal/index.tsx
+++ personal-site/src/pages/Journal/index.tsx
@@ -1,6 +1,7 @@
 import PageHeader from '@/components/blog/PageHeader';
 import Page from '@/components/layout/Page';
 import { getAllTitles } from '@/utils/services/JournalServices';
+import Highlight from '@/components/ui/Highlight';
 
 export default function Journal() {
     const titles = getAllTitles();
@@ -9,18 +10,18 @@ export default function Journal() {
         <Page>
             <PageHeader subtitle="Where I write dumb stuff and random thoughts">Journal</PageHeader>
 
-            {titles.map(note => (
-                <div key={note.slug}>
-                    <h2>
-                        {note.title + ' -> '}
-                        <a href={`/journal/${note.slug}`} class="text-blue-500 hover:underline">
-                            Read more
-                        </a>
-                    </h2>
-                </div>
-            ))}
-
-            <p>heyy :)</p>
+            <div className="flex flex-col gap-4 items-start">
+                {titles.map(note => (
+                    <Highlight
+                        className=""
+                        key={note.slug}
+                        href={`/journal/${note.slug}`}
+                        before={{ bgColor: '#FF9800', bgOpacity: 0.3 }}
+                        after={{ bgColor: '#FF9800', bgOpacity: 0.5 }}>
+                        {note.title}
+                    </Highlight>
+                ))}
+            </div>
         </Page>
     );
 }
