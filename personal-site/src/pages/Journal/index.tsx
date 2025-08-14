b7dda8e5c54f604e65ade6ef21fbef38e43f8f38
sharqawy <sharqawy@gmail.com>
2025-08-14 08:57:55 +0300

    moving the key prop to the outer wrapper element and considering using a more semantic HTML element like <article>

--- personal-site/src/pages/Journal/index.tsx
+++ personal-site/src/pages/Journal/index.tsx
@@ -10,19 +10,18 @@ export default function Journal() {
 
             <div className="flex flex-col gap-4 items-start">
                 {getAllJournalsInfo().map(note => (
-                    <span>
+                    <article key={note.slug} className="flex items-center gap-2">
                         <Highlight
-                            key={note.slug}
                             href={`/journal/${note.slug}`}
                             before={{ bgColor: '#FF9800', bgOpacity: 0.3 }}
                             after={{ bgColor: '#FF9800', bgOpacity: 0.5 }}>
                             {note.title}
-                        </Highlight>{' '}
-                        -{' '}
+                        </Highlight>
+                        <span>-</span>
                         <time dateTime={note.date} className="text-gray-500">
                             {note.date}
                         </time>
-                    </span>
+                    </article>
                 ))}
             </div>
         </Page>
