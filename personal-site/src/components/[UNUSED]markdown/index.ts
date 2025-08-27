655478f16c4cfbef12cd25036000448e79ce0c8b
sharqawycs <sharqawycs@gmail.com>
2025-08-27 10:29:24 +0300

    RENAMES

--- personal-site/src/components/[UNUSED]markdown/index.ts
+++ personal-site/src/components/[UNUSED]markdown/index.ts
@@ -3,6 +3,6 @@ import Paragraph from './Paragraph';
 import Link from './Link';
 import { List, ListItem } from './List';
 import { CodeBlock, InlineCode } from './Code';
-import Divider from '../ui/Divider';
+import Divider from '../ui/[UNUSED]Divider';
 
 export { Heading, Paragraph, Link, List, ListItem, CodeBlock, InlineCode, Divider };
