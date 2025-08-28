e37eb4488ea46ecd57a5125444259b82fd2ca889
sharqawycs <sharqawycs@gmail.com>
2025-08-28 21:44:56 +0300

    refactor Header component for consistent padding

--- personal-site/src/components/layout/Header.tsx
+++ personal-site/src/components/layout/Header.tsx
@@ -1,7 +1,7 @@
 const Header = () => {
   return (
     <>
-      <header className="bg-background/20 sticky top-0 z-40 px-4 py-2 backdrop-blur-md">
+      <header className="bg-background/20 sticky top-0 z-40 p-4 backdrop-blur-md">
         <div className="relative flex items-center justify-between">
           <a href="/">SHARQAWY</a>
           <nav className="flex items-center gap-4">
