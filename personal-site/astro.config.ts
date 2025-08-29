a9186d6819e75493a0f9af1ee8e1b623d7929114
sharqawycs <sharqawycs@gmail.com>
2025-08-30 00:09:19 +0300

    feat: add vite-plugin-compression for improved asset compression

--- personal-site/astro.config.ts
+++ personal-site/astro.config.ts
@@ -5,19 +5,45 @@ import sitemap from '@astrojs/sitemap';
 import tailwindcss from '@tailwindcss/vite';
 import { defineConfig } from 'astro/config';
 import { fileURLToPath, URL } from 'node:url';
+import compress from 'vite-plugin-compression';
+import htmlMinifier from 'vite-plugin-html-minifier';
+
+const sitemapConfig = sitemap({
+  filenameBase: 'sitemap',
+});
+
+const htmlMin = htmlMinifier({
+  minify: true,
+});
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://www.sharq.tech',
-  integrations: [mdx(), preact(), sitemap()],
+  integrations: [mdx(), preact(), sitemapConfig],
+
   vite: {
-    plugins: [tailwindcss()],
+    plugins: [tailwindcss(), compress(), htmlMin],
     resolve: {
       alias: {
         '@': fileURLToPath(new URL('./src', import.meta.url)),
       },
     },
+
+    build: {
+      minify: 'terser',
+      terserOptions: {
+        format: {
+          comments: false, // remove all comments
+        },
+      },
+      rollupOptions: {
+        output: {
+          manualChunks: undefined, // force single chunk
+        },
+      },
+    },
   },
+
   markdown: {
     shikiConfig: {
       theme: 'github-dark',
