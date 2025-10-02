3f9430fc460dc88abfdc02e7a9f93064dc5e45bc
sharqawycs <sharqawycs@gmail.com>
2025-10-02 21:43:30 +0300

    Add environment variables for site configuration and update layout to use them

---
+++ env.d.ts
@@ -0,0 +1,11 @@
+interface ImportMetaEnv {
+  readonly PUBLIC_DEFAULT_TITLE: string;
+  readonly PUBLIC_DEFAULT_DESCRIPTION: string;
+  readonly PUBLIC_OG_IMAGE: string;
+  readonly PUBLIC_POSTFIX: string;
+  readonly BASE_URL: string;
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
