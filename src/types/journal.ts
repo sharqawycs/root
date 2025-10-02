dd2084e0a4bbbe5e0370a073fd820f4b6cd5cb00
sharqawycs <sharqawycs@gmail.com>
2025-10-02 20:47:42 +0300

    Refactor Journal type to improve type safety and update usage in journal.astro

--- src/types/journal.ts
+++ src/types/journal.ts
@@ -1,12 +1,16 @@
-type Journal = {
-  slug: string;
-  data: {
-    title: string;
-    date: Date;
-    description?: string;
-    tags?: string[];
-    image?: string;
-  };
-};
+import type { CollectionEntry } from 'astro:content';
+
+type Journal =
+  | {
+      slug: string;
+      data: {
+        title: string;
+        date: Date;
+        description?: string;
+        tags?: string[];
+        image?: string;
+      };
+    }
+  | CollectionEntry<'journal'>;
 
 export type { Journal };
